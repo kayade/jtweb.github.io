1551b8d4d65e3cc4eb9007ef9b8bee6590f65352
kayade <42187663+kayade@users.noreply.github.com>
2022-09-20 11:22:01 +0100

    last changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AlertController, Platform} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  private bg: string;
+  private devWidth: any;
+
+  constructor(private plt: Platform, private alertController: AlertController) {
+
+    this.bg = './../../assets/lg.svg';
+
+    this.devWidth = this.plt.width();
+
+    //this.displayAlert('this.devWidth:' + this.devWidth);
+  }
+
+  async displayAlert(mess) {
+    const alert = await this.alertController.create({
+      message: mess,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 
 }
